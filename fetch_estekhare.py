1a332c7b4c832f6187361b9eddf236d452a75f65
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-04-26 14:23:01 +0430

    The word "sure" removed from sure name.

--- fetch_estekhare.py
+++ fetch_estekhare.py
@@ -1,12 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
+from sqlite3 import Error
 
 
-def get_estekhare_data(soup, container_box):
+def parse_estekhare_data(soup, container_box):
+    """
+    Parses the html from estekhare.net
+    """
     container = soup.find('div', {'id': container_box})
     result = container.find('h1').text
     address = container.find('h6').text.split()
-    print(address)
     if str.endswith(container_box, '1'):
         # finglish
         sure_no = int(address[0][6:-1])
@@ -21,7 +25,8 @@ def get_estekhare_data(soup, container_box):
         aye_no = int(address[1][6:-1])
     h2s = container.find_all('h2')
     comment = h2s[0].text
-    sure = h2s[1].contents[0]
+    sure = h2s[1].contents[0].split()
+    sure = ' '.join(sure[1:])
     aye = h2s[1].contents[2]
     return {
         'result': result,
@@ -33,18 +38,21 @@ def get_estekhare_data(soup, container_box):
     }
 
 
-url = 'http://estekhare.net/index2.php'
-re = requests.get(url)
-soup = BeautifulSoup(re.text, 'html.parser')
+def get_estekhare_data():
+    url = 'http://estekhare.net/index2.php'
+    re = requests.get(url)
+    soup = BeautifulSoup(re.text, 'html.parser')
 
-finglish_box = 'newboxes1'
-farsi_box = 'newboxes2'
-english_box = 'newboxes3'
+    finglish_box = 'newboxes1'
+    farsi_box = 'newboxes2'
+    english_box = 'newboxes3'
 
-finglish = get_estekhare_data(soup, finglish_box)
-farsi = get_estekhare_data(soup, farsi_box)
-english = get_estekhare_data(soup, english_box)
+    finglish = parse_estekhare_data(soup, finglish_box)
+    farsi = parse_estekhare_data(soup, farsi_box)
+    english = parse_estekhare_data(soup, english_box)
 
-print(finglish)
-print(farsi)
-print(english)
+    return {
+        'finglish': finglish,
+        'farsi': farsi,
+        'english': english
+    }
